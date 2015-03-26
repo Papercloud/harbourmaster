1a9dbc0d06f7f133debdb0f75527099aae26f830
Isaac Norman <isaacdnorman@gmail.com>
2015-03-26 12:06:39 +1100

    Moved the generators to the correct spot. And updated the cntroller generator so that it doesn't permit created_at and updated_at

--- lib/harbourmaster/generators/harbourmaster/controller/templates/controller.rb
+++ lib/harbourmaster/generators/harbourmaster/controller/templates/controller.rb
@@ -2,10 +2,11 @@ class <%= plural_name.camelize %>Controller < BaseApiController
   <%= "actions :#{options['actions'].join(', :') }" if options['actions'].present? -%>
 
 <% if options['permit_params'] -%>
+
   private
 
   def permitted_params
-    params.require(:<%= name.underscore -%>).permit(:<%= (class_name.constantize.new.attribute_names).join(', :') %>)
+    params.require(:<%= name.underscore -%>).permit(:<%= (class_name.constantize.new.attribute_names - ["created_at", "updated_at"]).join(', :') %>)
   end
 <% end -%>
 end
--- spec/dummy/app/controllers/api/users_controller.rb
+++ spec/dummy/app/controllers/api/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < BaseApiController
   actions :index, :show, :create, :update, :destroy
+
   private
 
   def permitted_params
-    params.require(:user).permit(:id, :name, :email, :created_at, :updated_at)
+    params.require(:user).permit(:id, :name, :email)
   end
 end
