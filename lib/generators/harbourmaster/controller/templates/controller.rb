be32c165e7ec2204828785416b6acfa03a914fa3
Isaac Norman <isaacdnorman@gmail.com>
2015-05-25 14:43:58 +1000

    Changed the permitted params in the generator to reflect inherited resources defaults

--- lib/generators/harbourmaster/controller/templates/controller.rb
+++ lib/generators/harbourmaster/controller/templates/controller.rb
@@ -10,7 +10,7 @@ class <%= ("#{api_base_route}/#{plural_name}".split('/') - ['app', 'controllers'
 
   private
 
-  def permitted_params
+  def <%= name.underscore -%>_params
     params.require(:<%= name.underscore -%>).permit(:<%= (class_name.constantize.new.attribute_names - ["created_at", "updated_at"]).join(', :') %>)
   end
 <% end -%>
